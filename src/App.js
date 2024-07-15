609d919ddd3ac202938d769fcc520100723afafa
Cyanider <amihasan420@gmail.com>
2024-07-15 17:49:07 +0600

    Day 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex m-1 p-2">
+     <div className="m-1 p-2">
+      Messeges
+     </div>
+     <div className="m-1 p-2">
+      Hasan
+     </div>
+    </div>
+  );
+}
+
+export default App;
