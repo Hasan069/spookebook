523f0265768e6ef280956815d127f835851f6761
Cyanider <amihasan420@gmail.com>
2024-07-15 20:16:45 +0600

    Day 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ function App() {
   return (
     <body>
       <div className="h-24 flex justify-between m-2 p-2 border-2 border-purple-700 rounded-lg">
-        <div className="w-1/4 border-2 border-black rounded-lg p-2 bg-purple-700 text-neutral-50">
+        <div className="w-1/4 border-2 border-black rounded-lg p-2 bg-new text-neutral-50">
           Messege
         </div>
-        <div className="w-3/4 border-2 border-black rounded-lg p-2 bg-purple-700 text-neutral-50">
+        <div className="w-3/4 border-2 border-black rounded-lg p-2 bg-new text-neutral-50">
           Hasan
         </div>
       </div>
+      
+      <div className="flex justify-between">
+      <div className="m-2 p-2 h-20 w-1/3 bg-new rounded-lg align-middle text-neutral-50  ">
+        Tanvir
+      </div>
+      <div>
+        Go Ahead! 👍🏼
+      </div>
+      </div>
+
     </body>
   );
 }
