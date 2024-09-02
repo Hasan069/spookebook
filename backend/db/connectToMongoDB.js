1f43a2de6185ca7a03ad3907ead2e2730f375fbb
Cyanider <amihasan420@gmail.com>
2024-09-02 16:52:56 +0600

    Day 8

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config({path:'.env'});
-
-
-
-
-
-// await mongoose.connect(process.env.MONGO_DB_URI);
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://amihasan420:tjjI8pHVHAOo7yHS@cluster0.ruehj.mongodb.net/spookebook?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_DB_URI);
     console.log("Connected to the MongoDB");
   } catch (error) {
     console.log("Error connecting to the database", error.message);
@@ -18,3 +12,5 @@ const connectToMongoDB = async () => {
 };
 
 export default connectToMongoDB;
+
+// await mongoose.connect("mongodb+srv://amihasan420:tjjI8pHVHAOo7yHS@cluster0.ruehj.mongodb.net/spookebook?retryWrites=true&w=majority&appName=Cluster0");
