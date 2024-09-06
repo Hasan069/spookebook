2b5da7a23cd2a1cf11d1a65db98840ca1d02ee0b
Cyanider <amihasan420@gmail.com>
2024-09-06 17:37:51 +0600

    Day 11

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,31 @@
+import avatar from "../../assets/avatar.jpeg";
+
+const Message = () => {
+  return (
+    <div>
+      <div className="flex gap-2 items-center rounded-2xl p-2 py-4 cursor-pointer ">
+        <div className="avatar">
+          <div className="  w-14 rounded-full ring-inset-2 ring-2 ring-orange-800">
+            <img src={avatar} alt="Avatar" className="rounded-full" />
+          </div>
+        </div>
+        <div class="flex flex-col w-full min-w-[420px] max-w-[420px] leading-1.5 p-4 border-gray-200 bg-orange-800 rounded-e-3xl rounded-es-3xl ">
+          <div class="flex items-center space-x-2 rtl:space-x-reverse ">
+            <span class="text-sm font-semibold text-gray-900 dark:text-white hover:text-black">
+              Tanvir Hasan Serazy
+            </span>
+            <span class="text-sm font-normal  dark:text-gray-400">11:46</span>
+          </div>
+          <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white text-justify">
+            Press Win_key + : for Emojis 🐸
+          </p>
+          <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+            Delivered
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import MessegeContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className=" flex rounded-xl bg-clip-padding p-1 backdrop-filter backdrop-blur-lg bg-opacity-50 max-h-[80vh] ">
       <Sidebar />
+      <MessegeContainer/>
     </div>
   );
 };
