66c23406ad81a533189e053295cd2cbf88c744c9
Cyanider <amihasan420@gmail.com>
2024-09-08 18:13:57 +0600

    Day 12

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { RiLogoutCircleLine } from "react-icons/ri";
-
+import useLogout from "../../hooks/useLogout";
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
   return (
     <div>
       <div className="mt-auto">
-        <RiLogoutCircleLine className="h-10 w-10 bg-transparent text-gray-800  m-2 rounded-2xl hover:bg-red-800 cursor-pointer" />
+        {!loading ? (
+          <RiLogoutCircleLine
+            className="h-10 w-10 bg-transparent text-gray-800  m-2 rounded-2xl hover:bg-red-800 cursor-pointer"
+            onClick={logout}
+          />
+        ) : (
+          <span class="relative flex h-3 w-3">
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+          </span>
+        )}
       </div>
     </div>
   );
